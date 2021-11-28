990edff8e6b956de10ada45a8a0a597e69b135fb
Rosalia Spatoliatore <rosalia.spatoliatore@gmail.com>
2021-11-28 22:05:32 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'exercise-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'exercise-angular-pt2';
+}
